7d51605210a3521651164612ad754ab58330bac6
Victor Elias Vivanco Rivera <eliasvr2189@gmail.com>
2025-02-14 23:02:45 -0500

    Actualizandao

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const EcommerceDB = require('./Config/dataBase');
 const router = require('./Routes/router');
 
 const app = express();
-
+const PORT = process.env.PORT || 4002;
 EcommerceDB();
 
 app.use(express.json());
 app.get('/', (req, res) => res.send('Api ejecutando'));
 app.use('/Ecommerce/api', router); 
 
-const PORT = process.env.PORT || 4002;
+
 app.listen(PORT, () => console.log(`Servidor iniciado en el puerto: ${PORT}`));
